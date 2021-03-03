13be1208c5f981334f0a992e0f6d5e3cc5f4a293
nicholas held <nicholaspheld@gmail.com>
2021-03-03 10:00:45 -0600

    updated JS prompts after tutor session

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Assignment Code - Given line 2
 var generateBtn = document.querySelector("#generate");
 
-var lowercase = ["a","b","c","d","e","f","g", "h", "i", "j", "k", "l","m","n","o","p", "q", "r", "s","t", "u","v","w","x","y", "z"]
-var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-var numbers = ['1','2','3','4','5','6','7','8','9','0']
-var symbols = ['!','@','#','$','%','^','&','*','(',')','_','+']
-//var criteria - with all characters in array
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+var symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+']
 //var length of characters 8-128
+//var criteria - concat lowercase, uppercase, numbers, and symbols? >randomize
 //var passwordresult
 
 console.log(lowercase)
@@ -14,32 +14,54 @@ console.log(uppercase)
 console.log(numbers)
 console.log(symbols)
 
+function passwordOptions() {
+  var passwordLength = parseInt(prompt("how many characters would you like in the password?"))
 
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password length must be between 8 and 128 characters")
+    return;
+  }
 
-function generatePassword() {
- 
- 
- //1. generate pswd
- //2. prompt user
- //
- //sign = window.prompt('How many characters do you want in your password', 'enter number 8-128', amount);
-//store the user input on how many characters
- var characters =parseInt(window.prompt("Number of characters: 8-128", min =8, max =128));
-  alert("You entererd " + characters);  //alert for how many characters 8-128)
-  var lowercase =parseInt(window.alert("Shall we include lower case letters"));
-  var uppercase =parseInt(window.alert("Shall we include upper case letters"));
-  var numbers =parseInt(window.alert("Shall we include numbers"));
-  var symbols =parseInt(window.alert("Shall we include sybmols"));
-  //alert("You entererd " + uppercase);
-  return "password";//password gets returned where secure pswd location is
+  var hasSpecial = confirm("Click Okay to confirm special characters")
+
+  var hasNumbers = confirm("Click okay to include numbers")
+
+  var hasUpper = confirm("Click to include Uppercase letters")
+
+  var hasLower = confirm("Click to include lowercase letters")
+
+  if (hasSpecial === false &&
+    hasNumbers === false &&
+    hasUpper === false &&
+    hasLower === false) {
+    alert("Must select a character type")
+    return;
+  }
+
+  //{} represent object
+  var pwdOptions = {
+    pwdLength: passwordLength,
+    special: hasSpecial,
+    numbers: hasNumbers,
+    upper: hasUpper,
+    lower: hasLower
+  }
+
+  console.log(pwdOptions)
+  return pwdOptions;
+
 }
 
-function randomize()  {
-  for (var i=0; i < length; i ++) {
-    var special = Math.floor(Math.random() *(criteria.length));
-    console.log(criteria)
-    randompswd.push(criteria[special]);
-  }
+function generatePassword() {
+  var options = passwordOptions()
+
+}
+
+
+function getRandom(array) {
+  var randomIndex = Math.floor(Math.random() * array.length)
+  var randomElement = array[randomIndex]
+  return randomElement
 }
 
 // Write password to the #password input - Given
